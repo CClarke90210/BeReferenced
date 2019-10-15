a0b25f89f4e22105b8514c10095660fd502294b2
chadclarke15@gmail.com <chadclarke15@gmail.com>
2019-10-15 15:34:56 -0400

    First commit

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,11 +17,10 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <h1 className="display-3 mb-4">BeReferenced</h1>
                 <p className="lead">
                   {' '}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
+                Show recruiters, you're serious about the future of your career.
                 </p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
